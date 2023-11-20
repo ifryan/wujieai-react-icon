7103d688607c7e42f228c1fc68013b1c7ee13ba2
balge <wchengba@gmail.com>
2023-11-20 16:23:01 +0800

    删除 default props，使用默认值 (#53)
    * init
    
    * 依赖变更
    
    * pages添加version
    
    * fix Mixed up default and named imports
    
    * 删除 default props
    
    ---------
    
    Co-authored-by: wuchengba <wucb@8btc.com>

--- bin/template.js
+++ bin/template.js
@@ -26,29 +26,14 @@ const getAttrs = (style) => {
 
 const getElementCode = (ComponentName, attrs, svgCode) => `
   import React from 'react';
-  import PropTypes from 'prop-types';
-
   export const ${ComponentName} = (props) => {
-    const { color, size, ...otherProps } = props;
+    const { color="currentColor", size="24", ...otherProps } = props;
     return (
       <svg ${attrs}>
         ${svgCode}
       </svg>
     )
   };
-
-  ${ComponentName}.propTypes = {
-    color: PropTypes.string,
-    size: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ]),
-  }
-
-  ${ComponentName}.defaultProps = {
-    color: 'currentColor',
-    size: '24',
-  }
 `;
 
 module.exports = { getAttrs, getElementCode };
