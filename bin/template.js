40c353dfd76c367a3b6083b958a2cbc5847189ec
wuchengba <wucb@8btc.com>
2024-09-19 10:24:19 +0800

    生成双倍图

--- bin/template.js
+++ bin/template.js
@@ -1,11 +1,9 @@
-const { svgToBase64 } = require("./utils");
-
 const getAttrs = (style) => {
   const baseAttrs = {
     xmlns: "http://www.w3.org/2000/svg",
     width: "size",
     height: "size",
-    viewBox: "0 0 24 24",
+    viewBox: "0 0 48 48",
   };
   const fillAttrs = {
     fill: "color",
