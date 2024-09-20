b087208d136e75a05d12fb3d5ec63a594e32b1f3
wuchengba <wucb@8btc.com>
2024-09-20 12:43:32 +0800

    替换svg转base包

--- bin/version.js
+++ bin/version.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const packageJsonPath = "../package.json";
+const packageJsonPath = require("../package.json");
 
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
